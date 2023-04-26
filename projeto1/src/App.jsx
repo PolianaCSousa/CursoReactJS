255e17091befd53c3d29b2dd8906cf276f072d8c
Poliana <polianasamonte@gmail.com>
2023-04-26 16:28:38 -0300

    componente que retorna mais de um elemento html, ou retorna outro componente

--- projeto1/src/App.jsx
+++ projeto1/src/App.jsx
@@ -1,7 +1,17 @@
 //o nome do componente deve ser o mesmo que o nome do arquivo
 //outra convenção é colocar o nome do componente com a primeira letra maiúscula, para nomes compostos deixamos em CamelCase
+
+import Titulo from "./Titulo" //um componente pode usar outro, para isso é preciso importá-lo
+
+//para retornar mais de 1 componente, é preciso usar parênteses e colocá-los dentro de uma div
 function App() {
-  return <h1>Olá mundo!</h1>
+  return (
+    <div>
+      <Titulo/>
+      <Titulo/>
+      <Titulo/>
+    </div>
+  )
 }
 
 //eu preciso exportar meu componente para que ele seja renderizado
---
+++ projeto1/src/Titulo.jsx
@@ -0,0 +1,11 @@
+//para retornar mais de uma tag, é preciso colocá-las dentro de uma div que deve estar dentro de ()
+function Titulo(){
+    return (
+        <div>
+            <h1>Oi, eu sou um Título</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis vero, dicta soluta officiis ab itaque velit hic eum impedit tempore necessitatibus excepturi quaerat praesentium! Molestiae cumque laboriosam nostrum esse veritatis.</p>
+        </div>
+    )
+}
+
+export default Titulo
