202aef4a8d93fb2b3990dd9d44100d08ddabdb0a
Poliana <polianasamonte@gmail.com>
2023-04-26 16:14:04 -0300

    primeiro componente de função

---
+++ projeto1/src/App.jsx
@@ -0,0 +1,9 @@
+//o nome do componente deve ser o mesmo que o nome do arquivo
+//outra convenção é colocar o nome do componente com a primeira letra maiúscula, para nomes compostos deixamos em CamelCase
+function App() {
+  return <h1>Olá mundo!</h1>
+}
+
+//eu preciso exportar meu componente para que ele seja renderizado
+export default App //essa é a exportação padrão (olhar a outra depois)
+
