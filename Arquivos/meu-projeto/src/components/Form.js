f838fe1e8b1a5eda78e9d7a7e30030fed900203c
Poliana <polianasamonte@gmail.com>
2023-04-27 23:25:32 -0300

    Estados no React

--- Arquivos/meu-projeto/src/components/Form.js
+++ Arquivos/meu-projeto/src/components/Form.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 function Form(){
 
     function cadastrarUsuario(e){
@@ -5,12 +7,20 @@ function Form(){
         console.log('Cadastrou o usuário!')
     }
 
+    const [name, setName] = useState('Poliana')
+
     return(
         <div>
             <h1>Meu Cadastro:</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite o seu nome"></input>
+                    <label htmlFor="name">Nome:</label>
+                    <input type="text" id="name" name="name" placeholder="Digite o seu nome" onChange={(e) => {setName(e.target.value)}}></input>
+                </div>
+                <p>Nome Digitado: {name}</p>
+                <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input type="password" id="password" name="password" placeholder="Digite a sua senha"></input>
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar"></input>
