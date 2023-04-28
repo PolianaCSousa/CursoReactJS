2c91c4d6103640ab7f6d391e409071d2f3c7053f
Poliana <polianasamonte@gmail.com>
2023-04-27 23:08:32 -0300

    Eventos React

--- Arquivos/meu-projeto/src/App.js
+++ Arquivos/meu-projeto/src/App.js
@@ -1,21 +1,19 @@
 import './App.css';
 import './components/HelloWorld';
-import HelloWorld from './components/HelloWorld';
-import SayMyName from './components/SayMyName';
-import Pessoa from './Pessoa';
-import Frase from './components/Frase';
-import List from './components/List';
+import Eventos from './components/Eventos';
+import Form from './components/Form';
 
 function App() {
 
   return (
     <div className="App">   
-        <Frase/>
-        <HelloWorld/>
-        <Pessoa nome="Poliana" idade="23" profissao="Estudante"/>
-        <List/>
+      <h1>Criando Eventos</h1>
+      <Eventos numero={1}/>
+      <Eventos numero={2}/>
+      <Form/>
     </div>
-  );
+    
+  )
 }
 
 export default App;
