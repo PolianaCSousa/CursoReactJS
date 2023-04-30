193e7d94d717a1a3224d0dc302f8e1d9bf78c677
Poliana <polianasamonte@gmail.com>
2023-04-30 18:22:54 -0300

    Renderizando listas usando map

--- Arquivos/meu-projeto/src/App.js
+++ Arquivos/meu-projeto/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
-import Condicional from './components/Condicional';
-import './components/HelloWorld';
+import OutraLista from './components/OutraLista';
 
 
 function App() {
 
+  const meusItens = ['React', 'Vue', 'Angular']
+  
+
   return (
     <div className="App">   
-      <h1>Renderizaçaão Condicional</h1>
-      <Condicional/>
+      <h1>Renderização de Listas</h1>
+      <OutraLista itens={meusItens}/> 
+      <OutraLista itens={[]}/>     
     </div>
     
   )
